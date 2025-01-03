fd92bebe63bb9ff75e5563755e6954ded031348e
= <=>
2025-01-03 18:43:27 +0300

    first commit

---
+++ HomeVR/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeVR;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HomeVR/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HomeVR;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
